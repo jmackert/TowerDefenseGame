b96da840f3b4314664067501c0b0c84ab7988179
jmackert <jonahbmackert@gmail.com>
2022-04-17 21:25:09 -0400

    Added TempTower and Tower script
    Added a Temporary tower and projectile. Added a "Tower" script that enables the tower to find the closest enemy and target it. Shooting a projectile at the target and damaging the target is WIP

---
+++ Tower Defense Game/Assets/Scripts/Tower.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private float range = 2.5f;
+    private string enemyTag = "Enemy";
+    private float attackSpeed = 1f;
+    private void UpdateTarget(){
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance){
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if(nearestEnemy != null && shortestDistance <= range){
+            target = nearestEnemy.transform;
+        }else{
+            target = null;
+        }
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null){
+            return;
+        }
+    }
+}
