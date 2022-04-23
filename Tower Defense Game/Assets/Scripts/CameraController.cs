b60598aad384cb4371f2518ae756925b55ae8cba
jmackert <jonahbmackert@gmail.com>
2022-04-22 20:59:50 -0400

    Finished Camera Script
    Camera now pans, zooms, and rotates within set borders

--- Tower Defense Game/Assets/Scripts/CameraController.cs
+++ Tower Defense Game/Assets/Scripts/CameraController.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform cameraTransform;
+    private Quaternion NewRotation;
+    private Vector3 newPosition;
+    private Vector3 zoomAmount = new Vector3(0f,-1f,1f);
+    private Vector3 newZoom;
+    private Vector3 dragStartPosition;
+    private Vector3 dragCurrentPosition;
+    private Vector3 rotateStartPosition;
+    private Vector3 rotateCurrentPosition;
     private float panSpeed = 0.5f;
     private float rotationSpeed = 1f;
     private float movementTime = 5f;
-
-    private Quaternion NewRotation;
-    public Vector3 newPosition;
-    private Vector3 zoomAmount = new Vector3(0f,-1f,1f);
-    public Vector3 newZoom;
-    public Vector3 dragStartPosition;
-    public Vector3 dragCurrentPosition;
+    private float minZoom = -5f;
+    private float maxZoom = 20f;
+    private float minPan = 0f;
+    private float maxPan = 14f;
 
     private void Start() {
         newPosition = transform.position;
@@ -24,11 +29,15 @@ public class CameraController : MonoBehaviour
         HandleMovementInput();
     }
     private void HandleMouseInput(){
+        // Camera Zoom
         if(Input.mouseScrollDelta.y != 0){
             newZoom += Input.mouseScrollDelta.y * zoomAmount;
         }
-            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, movementTime * Time.deltaTime);
-        if(Input.GetMouseButtonDown(0)){
+        newZoom.y = Mathf.Clamp(newZoom.y, -minZoom, maxZoom);
+        newZoom.z = Mathf.Clamp(newZoom.z, -maxZoom, minZoom);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, movementTime * Time.deltaTime);
+        // Camera Pan
+        if(Input.GetMouseButtonDown(2)){
             Plane plane = new Plane(Vector3.up, Vector3.zero);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             float entry;
@@ -36,7 +45,7 @@ public class CameraController : MonoBehaviour
                 dragStartPosition = ray.GetPoint(entry);
             }
         }
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButton(2)){
             Plane plane = new Plane(Vector3.up, Vector3.zero);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             float entry;
@@ -45,6 +54,20 @@ public class CameraController : MonoBehaviour
                 newPosition = transform.position + dragStartPosition - dragCurrentPosition;
             }
         }
+        newPosition.x = Mathf.Clamp(newPosition.x, minPan, maxPan);
+        newPosition.z = Mathf.Clamp(newPosition.z, -maxPan, minPan);
+        transform.position = Vector3.Lerp(transform.position, newPosition, movementTime * Time.deltaTime);
+        // Camera Rotation
+        if(Input.GetMouseButtonDown(1)){
+            rotateStartPosition = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(1)){
+            rotateCurrentPosition = Input.mousePosition;
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+            rotateStartPosition = rotateCurrentPosition;
+            NewRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, NewRotation, movementTime * Time.deltaTime);
     }
     private void HandleMovementInput(){
         if(Input.GetKey(KeyCode.W)){
@@ -64,49 +87,6 @@ public class CameraController : MonoBehaviour
         }
         if(Input.GetKey(KeyCode.E)){
             NewRotation *= Quaternion.Euler(Vector3.up * -rotationSpeed);
-        }
-        transform.position = Vector3.Lerp(transform.position, newPosition, movementTime * Time.deltaTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, NewRotation, movementTime * Time.deltaTime);
-        
+        }   
     }
 }
-    /*public float panSpeed = 15f;
-    public float panBoarderThickness = 15f;
-    public float scrollSpeed = 5f;
-    public float minY = 10f;
-    public float maxY = 20f;
-    public float minX = 0f;
-    public float maxX = 15f;
-    public float minZ = -20f;
-    public float maxZ = -10f;    
-    public float rotateSpeed = 25f;
-
-    private void Update()
-    {
-        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBoarderThickness){
-            transform.Translate(Vector3.forward.normalized * panSpeed * Time.deltaTime, Space.World);
-        }
-        if(Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness){
-            transform.Translate(Vector3.back.normalized * panSpeed * Time.deltaTime, Space.World);
-        }
-        if(Input.GetKey("a") || Input.mousePosition.x <= panBoarderThickness){
-            transform.Translate(Vector3.left.normalized * panSpeed * Time.deltaTime, Space.World);
-        }
-        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness){
-            transform.Translate(Vector3.right.normalized * panSpeed * Time.deltaTime, Space.World);
-        }
-        if(Input.GetKey("q")){
-            transform.Rotate(Vector3.down.normalized * rotateSpeed * Time.deltaTime, Space.World);
-        }
-        if(Input.GetKey("e")){
-            transform.Rotate(Vector3.up.normalized * rotateSpeed * Time.deltaTime, Space.World);
-        }
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 pos = transform.position;
-
-        pos.y -= scroll * 700 * scrollSpeed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
-        transform.position = pos;
-    }*/
