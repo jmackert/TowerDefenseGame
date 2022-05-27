01d767f349e50c5e259543dbc3c04302d62d7e35
jmackert <jonahbmackert@gmail.com>
2022-05-26 21:51:19 -0400

    Added second level for second upgrade path

---
+++ Tower Defense Game/Assets/Scripts/Towers/ArcherTowers/ArcherTower_0_2_0.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherTower_0_2_0 : Tower
+{
+        public ArcherTower_0_2_0(){
+        this.towerName = "Archer Tower 0_2_0";
+        this.range = 3f;
+        this.fireRate = 1.5f;
+        this.fireCountdown = 0f;
+        this.goldCost = 100;
+        this.upgradeOneCost = 100;
+        this.upgradeTwoCost = 100;
+        this.upgradeThreeCost = 100;
+        this.sellValue = 50;
+    }
+
+    private void Start() {
+        buildManager.ShowTowerUI(this.gameObject, towerName, upgradeOneCost, upgradeTwoCost, upgradeThreeCost);
+    }
+}
