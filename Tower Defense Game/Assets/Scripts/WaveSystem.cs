2b6be1e0d094ceb35d4c417f6ca55c7d62c9b631
jmackert <jonahbmackert@gmail.com>
2022-06-19 15:28:26 -0400

    getting closer

--- Tower Defense Game/Assets/Scripts/WaveSystem.cs
+++ Tower Defense Game/Assets/Scripts/WaveSystem.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
-    [SerializeField] private WaveInfo[] waveInfoArray;
+    [SerializeField] private Wave[] waveArray;
     public Transform enemySpawnPoint;
     public Player player;
     public static int numEnemiesAlive = 0;
 
+    /*private void Update() {
+        foreach (WaveInfo waveInfo in waveInfoArray)
+        {
+            waveInfo.SpawnWave();
+        }
+    }*/
     [System.Serializable]
-    private class WaveInfo{
-        WaveSystem waveSystem;
-        [SerializeField] private GameObject[] enemyArray;
-        //public Transform enemySpawnPoint;
+    private class Wave{
+        [SerializeField] private WaveInfo[] waveInfoArray;
 
-        public void SpawnEnemies(){
-            foreach (GameObject enemyToSpawn in enemyArray){
-                numEnemiesAlive++;
-                Instantiate(enemyToSpawn, waveSystem.enemySpawnPoint.position, waveSystem.enemySpawnPoint.rotation);
-            } 
+        [System.Serializable]
+        private class WaveInfo{
+
+            [SerializeField] private EnemyInfo[] enemyInfoArray;
+            [System.Serializable]
+            private class EnemyInfo{
+
+                WaveSystem waveSystem;
+                //[SerializeField] private GameObject[] enemyArray;
+                [SerializeField] private GameObject enemy;
+                [SerializeField] private float timer;
+                [SerializeField] private int numEnemies;
+
+                public IEnumerator SpawnWave(){
+                for (int i = 0; i < numEnemies; i++)
+                {
+                    //SpawnEnemies();
+                    //Debug.Log("WAVE: " + i);
+                    yield return new WaitForSeconds(timer);
+                }
+                //waveNumber++;
+                //player.IncreaseRoundsSurvived();
+            }
+
+                /*private void SpawnEnemies(){
+                    foreach (GameObject enemyToSpawn in enemyArray){
+                        numEnemiesAlive++;
+                        Instantiate(enemyToSpawn, waveSystem.enemySpawnPoint.position, waveSystem.enemySpawnPoint.rotation);
+                    } 
+                }*/
+            }
         }
     }
 }
