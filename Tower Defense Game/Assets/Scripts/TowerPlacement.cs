4790a2d8f7be3f6fec39a4914b79fb73f5649445
jmackert <jonahbmackert@gmail.com>
2022-08-28 21:48:20 -0400

    Valid placement indicators working now
    next is to implement currency into new tower placement system

--- Tower Defense Game/Assets/Scripts/TowerPlacement.cs
+++ Tower Defense Game/Assets/Scripts/TowerPlacement.cs
@@ -7,27 +7,43 @@ public class TowerPlacement : MonoBehaviour
     [SerializeField] private Camera playerCamera;
     [SerializeField] private LayerMask placementCollideMask;
     [SerializeField] private LayerMask placementCheckMask;
+    [SerializeField] private Material validPlacementMaterial;
+    [SerializeField] private Material invalidPlacementMaterial;
     private GameObject currentPlacingTower;
     private GameObject currentTowerIndicator;
     private Ray ray;
     private RaycastHit hitInfo;
+    private MeshRenderer towerMaterial;
 
     void Update()
     {
         if(currentPlacingTower != null)
         {  
             MoveCurrentTowerToMouse();
+            ChangeIndicatorColor();
             PlaceCurrentTower();
             DeselectCurrentTower();
         }
     }
 
-    public void SetTowerToPlace(GameObject _tower){
+    public void SetTowerToPlace(GameObject _tower)
+    {
+        if(currentPlacingTower != null)
+        {
+            currentPlacingTower = null;
+        }
         currentPlacingTower = _tower;
     }
 
-    public void SetTowerIndicator(GameObject _currentTowerIndicator){
-        currentTowerIndicator = Instantiate(_currentTowerIndicator, Vector3.zero, Quaternion.identity);
+    public void SetTowerIndicator(GameObject _currentTowerIndicator)
+    {
+        if(currentTowerIndicator != null)
+        {
+            Destroy(currentTowerIndicator.gameObject);
+            currentTowerIndicator = null;
+        }
+        Vector3 pos = new Vector3 (100f,0f,0f);
+        currentTowerIndicator = Instantiate(_currentTowerIndicator, pos, Quaternion.identity);
     }
 
     private void MoveCurrentTowerToMouse()
@@ -38,7 +54,6 @@ public class TowerPlacement : MonoBehaviour
             currentTowerIndicator.transform.position = hitInfo.point;
         }
     }
-
     private void PlaceCurrentTower()
     {
         if(currentTowerIndicator != null)
@@ -77,6 +92,26 @@ public class TowerPlacement : MonoBehaviour
                 currentPlacingTower = null;
                 currentTowerIndicator = null;
             }
+        }
+    }
+
+    private void ChangeIndicatorColor()
+    {
+        BoxCollider towerCollider = currentTowerIndicator.gameObject.GetComponent<BoxCollider>();
+        towerCollider.isTrigger = true;
+
+        Vector3 boxCenter = currentTowerIndicator.gameObject.transform.position + towerCollider.center;
+        Vector3 halfExtents = towerCollider.size / 2;
+
+        if(Physics.CheckBox(boxCenter, halfExtents, Quaternion.identity, placementCheckMask, QueryTriggerInteraction.Ignore) || hitInfo.collider.gameObject.CompareTag("CantPlace"))                    
+        {
+            towerMaterial = currentTowerIndicator.gameObject.GetComponentInChildren<MeshRenderer>();
+            towerMaterial.material = invalidPlacementMaterial;
+        }
+        else
+        {
+            towerMaterial = currentTowerIndicator.gameObject.GetComponentInChildren<MeshRenderer>();
+            towerMaterial.material = validPlacementMaterial;
         }
     }
 }
